a80671ec51bc8a253a27f86047c0850d346f26a7
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2013-12-11 12:41:13 -0800

    We want a cleaner spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,20 +1,21 @@
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-require 'rspec'
-
-require 'active_model'
-require 'active_model/better_errors'
-
-# Requires supporting files with custom matchers and macros, etc,
-# in ./support/ and its subdirectories.
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
-
-RSpec.configure do |config|
+require "coveralls"
+Coveralls.wear! do
+  add_filter "/spec/"
 end
+
+require "pry"
+require "rspec"
+require "active_model"
+require "active_model/better_errors"
+
+require_relative "support/models"
 
 class String
   def ==(other)
-    return super other.to_s if other.is_a? ActiveModel::ErrorCollecting::ErrorMessage
-    super
+    if other.is_a? ActiveModel::ErrorCollecting::ErrorMessage
+      super other.to_s
+    else
+      super
+    end
   end
 end
